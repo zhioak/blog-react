ea9ff5b33cbb3d57dc1b2a6b317ca7e6682b6b0a
zhou <2080211280@qq.com>
2020-07-01 19:06:35 +0800

    修改bug

--- component/Banner.js
+++ component/Banner.js
@@ -1,7 +1,7 @@
 import '../static/style/component/banner.css'
 
 const Banner = ({ bg, title, desc }) => (
-    <div className="banner" style={bg && { backgroundImage: `url(${bg})` }}>
+    <div className="banner" style={{ backgroundImage: `url(${bg})` }}>
         <div className="banner-holder">
             <div className="banner-title">{title}</div>
             <div>{desc}</div>
