59ecba1e0d2126e27aa86b4a5fe509ca9ab1c39e
zhousb1999 <2080211280@qq.com>
2020-07-03 15:39:56 +0800

    修改首页title

--- component/Head.js
+++ component/Head.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 
 export default ({ title }) => (
   <Head>
-    <title>{title ? title + ' - ZHOU' : 'ZHOU'}</title>
+    <title>{title ? title + ' - ZHOU' : 'ZHOU 的自耕地'}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
     <link rel="icon" href="/static/img/favicon.ico" />
   </Head>
