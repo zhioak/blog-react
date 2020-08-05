907a37cf500d9772f52252ab0c8e419be935bd8b
zhou <2080211280@qq.com>
2020-08-05 15:31:51 +0800

    优化侧栏阴影，banner结构

--- component/Banner.js
+++ component/Banner.js
@@ -1,10 +1,8 @@
 import '../static/style/component/banner.css'
 const Banner = ({ bg, title, desc }) => (
     <div className="banner" style={bg ? { backgroundImage: `url(${bg})` } : {}} >
-        <div className="banner-holder">
-            <div className="banner-title">{title}</div>
-            <div>{desc}</div>
-        </div>
+        <div className="banner-title">{title}</div>
+        <div>{desc}</div>
     </div >
 )
 export default Banner
